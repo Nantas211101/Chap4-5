35e2af9874901c22d2824332127d5f257747b123
Thinh Vo Truong <thinhhochanh2003@gmail.com>
2023-04-26 18:34:24 +0700

    file takeInformation error done

--- include/Constants.hpp
+++ include/Constants.hpp
@@ -6,15 +6,15 @@ struct Constants {
                                    '5', '6', '7', '8', '9'};
     const int numOfAcceptedChar = 11;
 
-    // enum MAXI {
-    //     numOfNode = 7,
-    //     valueOfNode = 100,
-    // };
+    struct MAXI {
+        static constexpr int numOfNode = 7;
+        static constexpr int valueOfNode = 100;
+    };
 
-    // enum MINI {
-    //     numOfNode = 0,
-    //     valueOfNode = 0,
-    // };
+    struct MINI {
+        static constexpr int numOfNode = 0;
+        static constexpr int valueOfNode = 0;
+    };
 
     std::string infoError1 = "Only integer are accepted, number of node should in [0, 7]";
     std::string infoError2 = "Value of node should in [0, 100]";
